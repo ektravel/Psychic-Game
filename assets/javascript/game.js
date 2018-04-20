743dba702d3b622588260710b8b31da7f208e60e
ektravel <38017980+ektravel@users.noreply.github.com>
2018-04-19 22:39:37 -0500

    corrected code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,52 +19,53 @@ var wordsToGuessArr = [
 //an array of "blanks" for a randomly picked word
 var wordAnswerArr = [];
 
+//define starting value for remaining guesses
+var remainingGuesses = 12;
+
 //pick a random word 
 var wordToGuess = wordsToGuessArr[Math.floor(Math.random() * wordsToGuessArr.length)];
 
-//create an array of "blanks" for each word 
+//letters remaining to be guessed
+var remainingLetters = [];
+
+//create an array of "blanks" for each word and alert the user to pick a letter
 function fillBlanks(){
     for (var i = 0; i < wordToGuess.length; i++) {
         wordAnswerArr[i] = "_";
-    }
+    };
+    document.getElementById("lettersGuessed").innerHTML = wordAnswerArr.join("");
+    //note how many letters are remaining to be guessed
+    remainingLetters = wordToGuess.length;
+    alert("Guess a letter, Precious, won't you?");
 };
 
-//Use key events to listen for the letters that your players will type.
-
-// This function is run whenever the user presses a key.
+// This function runs whenever the user presses a key.
 document.onkeyup = function(event) {
-// Determines which key was pressed.
+    // Determines which key was pressed.
     var userGuess = event.key;
-}
-
-//note how many letters are remaining to be guessed
-var remainingLetters = wordsToGuess.length;
-//while there are still letters left to guess: display the number of guesses left
-//dispay the letters already guessed
-//prompt the user to enter new guesses 
-while(remainingLetters > 0) {
-    document.write(answerArray.join(" "));
-//show this message before user inputs a letter, check to make sure user's input is valid
-    var guess = prompt("Guess a letter, Precious, won't you?");
-//if the user does not want to play and clicks Cancel, the game ends
-    if (guess === null) {
-        break;
-    } 
-//if the user enters nothing or more than one letter, show this message
-    else if (guess.length !== 1) {
-        alert("It will pick a single letter, won't it? It will get the hose!");
+    remainingGuesses--;
+// if the user enters nothing or more than one letter, show this message
+   if (isNaN(userGuess) === false) {
+        alert("It will pick a letter, won't it? It will get the hose!");
     } 
     else {
-    //user picked a letter, apply the letter to the word, update the score with the guess
-        for (var j = 0; j < wordsToGuess.length; j++) {
-            if (wordsToGuess[j] === guess) {
-            wordAnswer[j] = guess;
-            //lower remaining letters by 1
-            remainingLetters--;
+        //user picked a letter, apply the letter to the word, update the score with the guess
+        for (var j = 0; j < wordToGuess.length; j++) {
+            
+            if (wordToGuess[j] === userGuess) {
+                //update "blank" with letter guessed
+                wordAnswerArr[j] = wordToGuess[j];
+                //lower remaining letters by 1
+                remainingLetters--;
+                document.getElementById("lettersGuessed").innerHTML = wordAnswerArr.join("");
             }
-        }
-    }
-}
-//show remaining guesses
-var remainingGuesses = document.getElementById("triesLeft");
+            else {
+                //lower remaining tries by 1
+                //show remaining guesses
+                document.getElementById("triesLeft").innerHTML = "Remaining guesses: " + remainingGuesses;
+            };
+            
+        };
 
+    };
+};
